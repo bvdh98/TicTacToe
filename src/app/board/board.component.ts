f6570574ade9565806ac10c3b4622c5577acc791
Ben <bvdhest1998@gmail.com>
2022-04-26 18:42:51 -0700

    created method to render x and os in cells WIP

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -7,14 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoardComponent implements OnInit {
   cells: string[];
+  isPlayerXTurn: boolean;
 
   constructor() {}
 
   ngOnInit(): void {
-    this.start()
+    this.start();
+  }
+
+  getPlayersTurn() {
+    return this.isPlayerXTurn ? 'X' : 'O';
+  }
+
+  updateCell(index: number): void {
+    if (!this.cells[index]) {
+      this.cells.splice(index,1,this.getPlayersTurn())
+      this.isPlayerXTurn = !this.isPlayerXTurn;
+    }
   }
 
   start() {
     this.cells = Array(9).fill(null);
+    this.isPlayerXTurn = true;
   }
 }
--- src/app/cell/cell.component.ts
+++ src/app/cell/cell.component.ts
@@ -2,11 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-cell',
-  template: `
-    <button [className]="'col-4'" *ngIf="!value">{{value}}</button>
-    <button [className]="'class' + 'x-button'" *ngIf="value === 'X'">{{value}}</button>
-    <button [className]="'class' + 'o-button'" *ngIf="value === 'O'">{{value}}</button>
-  `,
+  templateUrl:"./cell.component.html",
   styleUrls: ['./cell.component.css']
 })
 export class CellComponent implements OnInit {

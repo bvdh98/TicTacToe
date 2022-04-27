b330b7ae95b7ea80a2f273a5732dd70ae528e639
Ben <bvdhest1998@gmail.com>
2022-04-26 23:34:14 -0700

    implemented function to determine winner or draw WIP

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -19,9 +19,41 @@ export class BoardComponent implements OnInit {
     return this.isPlayerXTurn ? 'X' : 'O';
   }
 
+  playerXWon(r: number, c: number) {
+    let row = Array(3).fill(0);
+    let column = Array(3).fill(0);
+    let diag = Array(3).fill(0);
+    let leftDiag = Array(3).fill(0);
+
+    const board = [
+      this.cells.slice(0, 3),
+      this.cells.slice(3, 6),
+      this.cells.slice(6, 9),
+    ];
+    const boardSize = board[0].length;
+    row[r]++;
+    column[c]++;
+    if (r == c) {
+      diag[r]++;
+    }
+    if (r + c == boardSize) {
+      leftDiag[r]++;
+    }
+    if (
+      row[r] == boardSize ||
+      column[c] == boardSize ||
+      diag[r] == boardSize ||
+      leftDiag[r] == boardSize
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   updateCell(index: number): void {
     if (!this.cells[index]) {
-      this.cells.splice(index,1,this.getPlayersTurn())
+      this.cells.splice(index, 1, this.getPlayersTurn());
       this.isPlayerXTurn = !this.isPlayerXTurn;
     }
   }

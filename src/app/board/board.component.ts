ed01612920f301e8bd8d82b133b1ae5491a76215
Ben <bvdhest1998@gmail.com>
2022-04-27 02:58:05 -0700

    completed function to determine winner and or tie

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class BoardComponent implements OnInit {
   cells: string[];
   isPlayerXTurn: boolean;
+  winner: string | null;
 
   constructor() {}
 
@@ -19,6 +20,8 @@ export class BoardComponent implements OnInit {
     return this.isPlayerXTurn ? 'X' : 'O';
   }
 
+  /*I made this algorithm which is O(1) complexity
+  but I couldn`t figure out how to implement it*/
   playerXWon(r: number, c: number) {
     let row = Array(3).fill(0);
     let column = Array(3).fill(0);
@@ -56,10 +59,51 @@ export class BoardComponent implements OnInit {
       this.cells.splice(index, 1, this.getPlayersTurn());
       this.isPlayerXTurn = !this.isPlayerXTurn;
     }
+    this.winner = this.findWinner();
+  }
+
+  isTie() {
+    for (let i = 0; i < this.cells.length; i++) {
+      if (this.cells[i] === null) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  findWinner() {
+    const board = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < board.length; i++) {
+      const [a, b, c] = board[i];
+      console.log(a)
+      if (
+        this.cells[a] &&
+        this.cells[a] === this.cells[b] &&
+        this.cells[a] === this.cells[c]
+      ) {
+        return this.cells[a];
+      }
+    }
+    if (this.isTie()) {
+      return 'Tie';
+    }
+    else{
+      return null;
+    }
   }
 
   start() {
     this.cells = Array(9).fill(null);
+    this.winner = null;
     this.isPlayerXTurn = true;
   }
 }
